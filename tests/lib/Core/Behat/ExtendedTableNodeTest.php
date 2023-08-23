e93775433a790699df8b245b70880c0fadaff78a
Marek Nocoń <mnocon@users.noreply.github.com>
2023-08-23 11:33:29 +0200

    IBX-6230: Added validation of CSS selectors (#69)
    * Added validation of CSS selectors
    
    * Fix for special characters
    
    * Added :last-of-type detection

--- tests/lib/Core/Behat/ExtendedTableNodeTest.php
+++ tests/lib/Core/Behat/ExtendedTableNodeTest.php
@@ -15,7 +15,6 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
- *
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  *
  * @internal
