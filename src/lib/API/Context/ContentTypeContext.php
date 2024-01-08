ad59fb2dfec528fa961e4956e7dfe858d78d9be9
tomaszszopinski <tomasz.szopinski@ez.no>
2024-01-08 14:55:24 +0100

    IBX-7409: Adjusted behat step

--- src/lib/API/Context/ContentTypeContext.php
+++ src/lib/API/Context/ContentTypeContext.php
@@ -25,6 +25,7 @@ class ContentTypeContext implements Context
 
     /**
      * @Given I create a :contentTypeName Content Type in :contentTypeGroupName with :contentTypeIdentifier identifier
+     * @Given I create a :contentTypeName content type in :contentTypeGroupName with :contentTypeIdentifier identifier
      *
      * @param mixed $contentTypeName
      * @param mixed $contentTypeGroupName
