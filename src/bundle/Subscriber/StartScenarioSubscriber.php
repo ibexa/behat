3756aa3475c7dc5ec268103bbf532ad25dccdd3d
Michał Szołtysek <michal.szoltysek@ez.no>
2026-03-11 17:21:41 +0100

    fixup! [tmp][PHPStan] StartScenarioSubscriber.php

--- src/bundle/Subscriber/StartScenarioSubscriber.php
+++ src/bundle/Subscriber/StartScenarioSubscriber.php
@@ -13,26 +13,12 @@ use Behat\Behat\EventDispatcher\Event\ExampleTested;
 use Behat\Behat\EventDispatcher\Event\ScenarioTested;
 use Facebook\WebDriver\Exception\UnknownErrorException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 class StartScenarioSubscriber implements EventSubscriberInterface
 {
     private const RETRY_LIMIT = 2;
 
     public const PRIORITY = -1000;
-
-    private KernelInterface $kernel;
-
-    private int $width;
-
-    private int $height;
-
-    public function __construct(KernelInterface $kernel, int $width, int $height)
-    {
-        $this->kernel = $kernel;
-        $this->width = $width;
-        $this->height = $height;
-    }
 
     public static function getSubscribedEvents()
     {
@@ -45,28 +31,5 @@ class StartScenarioSubscriber implements EventSubscriberInterface
     public function resizeWindow(BeforeScenarioTested $event): void
     {
         // No usages, so simplest fix is to remove the function
-    }
-
-    /**
-     * @template T
-     *
-     * @param callable(mixed ...$args): T $fn
-     *
-     * @return T
-     */
-    private function executeWithRetry(callable $fn)
-    {
-        $counter = 0;
-        while (true) {
-            try {
-                return $fn();
-            } catch (UnknownErrorException $e) {
-                if ($counter > self::RETRY_LIMIT) {
-                    throw $e;
-                }
-                ++$counter;
-                usleep(100000 * 2 ** $counter);
-            }
-        }
     }
 }
