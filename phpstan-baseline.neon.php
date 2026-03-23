cf4069f7997a7792366013d71d6fe35beccddb8f
Michał Szołtysek <michal.szoltysek@ez.no>
2026-03-23 10:44:35 +0100

    [PHPStan] Split baselines

---
+++ phpstan-baseline.neon.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+$includes = [];
+
+if (PHP_VERSION_ID >= 80300) {
+    $includes[] = __DIR__ . '/phpstan-baseline-.neon';
+} else {
+    $includes[] = __DIR__ . '/phpstan-baseline-7.4.neon';
+}
+
+return [
+    'includes' => $includes,
+];
